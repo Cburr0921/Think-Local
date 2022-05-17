e3882971d37197f69403b4e2db7ec0ce2a75ae1e
DavZep <93220182+DavZep@users.noreply.github.com>
2022-05-17 11:35:50 -0700

    create foursquare fetch request

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,21 @@
-console.log("christian");
+// Configuration ==============================================
+var qSearch = "pizza"
+var userLocation = "los angeles"
 
-console.log("david");
+
+const options = {
+    method: 'GET',
+    headers: {
+      Accept: 'application/json',
+      Authorization: 'fsq3tG/J+x68WkLO19crXTBbrnDq/aCfvCO5YHaVAIeMPHs='
+    }
+  };
+  
+  fetch(`https://api.foursquare.com/v3/places/search?query=${qSearch}&near=${userLocation}`, options)
+    .then(res => res.json())
+    .then(data => {
+        console.log(data.results[0])
+    
+    })
+
+    .catch(err => console.error(err))
